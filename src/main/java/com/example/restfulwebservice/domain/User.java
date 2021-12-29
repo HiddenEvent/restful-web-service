2b9fd653cbdc696dd91884ecb6154b03e104839b
RichardKim <hiddeneventor@gmail.com>
2021-12-29 21:02:01 +0900

    SF-7 Filtering 처리 -jsonIgnore 처리로 데이터 필터링 처리 완료

--- src/main/java/com/example/restfulwebservice/domain/User.java
+++ src/main/java/com/example/restfulwebservice/domain/User.java
@@ -1,6 +1,8 @@
 
 package com.example.restfulwebservice.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +14,13 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(value = {"password"})
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
     @Past //현재 날짜 로부터 과거인 경우만 허용
     private Date joinDate;
+    private String password;
+    private String ssn;
 }
