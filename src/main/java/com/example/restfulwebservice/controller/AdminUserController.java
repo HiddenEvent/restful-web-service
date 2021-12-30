246d4b9d129fef925da6d98fcbee68795dc1a862
RichardKim <hiddeneventor@gmail.com>
2021-12-30 19:56:49 +0900

    SF-8 RestApi 버전관리 방법 1. uri를 통한 버전관리 테스트

--- src/main/java/com/example/restfulwebservice/controller/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/controller/AdminUserController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice.controller;
 
 import com.example.restfulwebservice.domain.User;
+import com.example.restfulwebservice.domain.UserV2;
 import com.example.restfulwebservice.exception.UserNotFoundException;
 import com.example.restfulwebservice.service.UserDaoService;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
@@ -8,6 +9,7 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
@@ -20,11 +22,11 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin/user")
+@RequestMapping("/admin")
 public class AdminUserController {
     private final UserDaoService service;
 
-    @GetMapping("")
+    @GetMapping("/user")
     public MappingJacksonValue retrieveAllUsers() {
         List<User> users = service.findAll();
         SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name","password", "ssn");
@@ -34,7 +36,7 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/v1/user/{id}")
     public MappingJacksonValue retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
         if (user == null) {
@@ -47,7 +49,24 @@ public class AdminUserController {
         return mapping;
     }
 
-    @PostMapping("")
+    @GetMapping("/v2/user/{id}")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("ID [" + id + "] not found");
+        }
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2); /* 공통된 속성이 있을 경우 객체의 속성을 복사하는 기능*/
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name","joinDate", "grade");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
+        mapping.setFilters(filters);
+        return mapping;
+    }
+
+    @PostMapping("/user")
     public ResponseEntity<User> creatUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         URI lcoation = ServletUriComponentsBuilder.fromCurrentContextPath()
