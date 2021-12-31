016f92da46374e3ee142f17116151f4af46f2a94
RichardKim <hiddeneventor@gmail.com>
2021-12-31 23:20:12 +0900

    SF-9 HATEOAS 학습 - 현재 리소스와 연관된(호출 가능한) 자원 상태 정보를 제공 예) 글을 등록하고 → 전체목록을 가져오는 URL을 response 데이터로 함께 전달한다.

--- src/main/java/com/example/restfulwebservice/controller/UserController.java
+++ src/main/java/com/example/restfulwebservice/controller/UserController.java
@@ -5,14 +5,20 @@ import com.example.restfulwebservice.exception.UserNotFoundException;
 import com.example.restfulwebservice.service.UserDaoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -24,14 +30,38 @@ public class UserController {
     public List<User> retrieveAllUsers() {
         return service.findAll();
     }
+
     @GetMapping("/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
         if (user == null) {
-            throw new UserNotFoundException("ID ["+id+"] not found");
+            throw new UserNotFoundException("ID [" + id + "] not found");
         }
-        return user;
+        // HATEOAS
+        EntityModel<User> entityModel = EntityModel.of(user);
+        Link link = linkTo(
+                methodOn(this.getClass()).retrieveAllUsers()
+        ).withRel("all-user");
+        entityModel.add(link);
+
+        return entityModel;
     }
+    // 전체 사용자 목록
+//    @GetMapping("/users2")
+//    public ResponseEntity<CollectionModel<EntityModel<User>>> retrieveUserList2() {
+//        List<EntityModel<User>> result = new ArrayList<>();
+//        List<User> users = service.findAll();
+//
+//        for (User user : users) {
+//            EntityModel entityModel = EntityModel.of(user);
+//            entityModel.add(linkTo(methodOn(this.getClass()).retrieveAllUsers()).withSelfRel());
+//
+//            result.add(entityModel);
+//        }
+//
+//        return ResponseEntity.ok(CollectionModel.of(result, linkTo(methodOn(this.getClass()).retrieveAllUsers()).withSelfRel()));
+//    }
+
 
     @PostMapping("")
     public ResponseEntity<User> creatUser(@Valid @RequestBody User user) {
@@ -48,7 +78,7 @@ public class UserController {
     public void deleteUser(@PathVariable int id) {
         User user = service.deleteById(id);
         if (user == null) {
-            throw new UserNotFoundException("ID ["+id+"] not found");
+            throw new UserNotFoundException("ID [" + id + "] not found");
         }
 
     }
